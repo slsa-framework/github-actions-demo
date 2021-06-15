86439f2a3f6e2bddb608860b1895310a9fcb06a1
Matthew Suozzo <msuozzo@google.com>
2021-06-15 13:13:43 -0400

    Use pointer instead of omitempty.

--- create_provenance.go
+++ create_provenance.go
@@ -64,7 +64,7 @@ type Recipe struct {
 	DefinedInMaterial int             `json:"definedInMaterial"`
 	EntryPoint        string          `json:"entryPoint"`
 	Arguments         json.RawMessage `json:"arguments"`
-	Environment       AnyContext      `json:"environment,omitempty"`
+	Environment       *AnyContext     `json:"environment"`
 }
 type Completeness struct {
 	Arguments   bool `json:"arguments"`
